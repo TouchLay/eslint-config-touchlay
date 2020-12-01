a6f07dd67a6916043fea89b08f8a7f9c852e4d96
Daniel Bugl <me@omnidan.net>
2020-12-01 19:01:17 +0100

    1.2.0: add security rules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   "parser": "babel-eslint",
   "extends": [ "standard", "standard-react", "plugin:flowtype/recommended" ],
-  "plugins": [ "standard", "react", "flowtype", "jest" ],
+  "plugins": [ "standard", "react", "flowtype", "jest", "react-security", "no-unsanitized" ],
   "rules": {
     "comma-dangle": [2, {
       "arrays": "always-multiline",
@@ -21,7 +21,18 @@ module.exports = {
     "react/require-default-props": [0],
     "react/no-multi-comp": [2, { "ignoreStateless": true }],
     "react/jsx-curly-brace-presence": [0],
-    "react/jsx-curly-newline": [1, { multiline: "consistent", singleline: "consistent" }]
+    "react/jsx-curly-newline": [1, { multiline: "consistent", singleline: "consistent" }],
+    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
+    "no-danger": "warn",
+    "no-find-dom-node": "warn",
+    "jsx-no-script-url": "warn",
+    "jsx-no-target-blank": "warn",
+    "jsx-props-no-spreading": "warn",
+    // https://github.com/snyk-labs/eslint-plugin-react-security
+    "no-refs": "warn",
+    // https://github.com/mozilla/eslint-plugin-no-unsanitized
+    "no-unsanitized/method": "error",
+    "no-unsanitized/property": "error",
   },
   "env": {
     "es6": true,
